f821db77a8753375f5c88708bd45856f30418074
Valeriy Emelyanov <emelva@rambler.ru>
2021-12-01 20:10:18 +0300

    Start

---
+++ src/main/java/com/example/jwtserving/JwtServingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtserving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtServingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtServingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtserving/JwtServingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtserving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtServingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
